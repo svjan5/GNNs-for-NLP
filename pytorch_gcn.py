56628443b71399fe2374f33632f20476628c9c46
shikhar <shikharvashishth@gmail.com>
2019-10-04 17:53:30 +0530

    removed extra lines

--- pytorch_gcn.py
+++ pytorch_gcn.py
@@ -1,14 +1,9 @@
 import os.path as osp
-
 import torch
 import torch.nn.functional as F
 from torch_geometric.datasets import Planetoid
 import torch_geometric.transforms as T
-from torch_geometric.nn import GCNConv, ChebConv  # noqa
-
-
-from utils import *
-import tensorflow as tf
+from torch_geometric.nn import GCNConv
 
 
 class KipfGCN(torch.nn.Module):
@@ -256,40 +251,3 @@ if __name__== "__main__":
 	model = Main(args)
 	model.fit()
 	print('Model Trained Successfully!!')
-
-
-"""
-dataset = 'Cora'
-
-
-
-
-
-
-
-
-def train():
-	
-
-
-def test():
-	model.eval()
-	logits, accs = model(), []
-	for _, mask in data('train_mask', 'val_mask', 'test_mask'):
-		pred = logits[mask].max(1)[1]
-		acc = pred.eq(data.y[mask]).sum().item() / mask.sum().item()
-		accs.append(acc)
-	return accs
-
-
-best_val_acc = test_acc = 0
-for epoch in range(1, 201):
-	train()
-	train_acc, val_acc, tmp_test_acc = test()
-	if val_acc > best_val_acc:
-		best_val_acc = val_acc
-		test_acc = tmp_test_acc
-	log = 'Epoch: {:03d}, Train: {:.4f}, Val: {:.4f}, Test: {:.4f}'
-	print(log.format(epoch, train_acc, best_val_acc, test_acc))
-
-"""
