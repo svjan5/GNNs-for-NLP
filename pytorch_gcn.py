cefc7358c465c1e8d5336077e3914d85dc5207d4
Shikhar Vashishth <shikharvashishth@gmail.com>
2019-11-04 11:19:30 +0800

    Update pytorch_gcn.py

--- pytorch_gcn.py
+++ pytorch_gcn.py
@@ -1,6 +1,6 @@
-import os.path as osp, argparse, time
+import os.path as osp
+from helper import *
 import torch
-import numpy as np
 import torch.nn.functional as F
 from torch_geometric.datasets import Planetoid
 import torch_geometric.transforms as T
